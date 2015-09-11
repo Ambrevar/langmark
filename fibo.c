62cda51de7e3a47fdfb6d84cf9a67cbb3487898e
Pierre Neidhardt <ambrevar@gmail.com>
2015-09-11 16:46:11 +0200

    c/cpp: Format

--- fibo.c
+++ fibo.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-unsigned long
-fibo (unsigned long n)
-{
-    if (n < 2)
-        return n;
-    else
-        return fibo(n-1) + fibo(n-2);
+unsigned long fibo(unsigned long n) {
+	if (n < 2) {
+		return n;
+	} else {
+		return fibo(n - 1) + fibo(n - 2);
+	}
 }
 
-int
-main (int argc, char** argv)
-{
-    printf ("%ld\n", fibo (atol (argv[1])));
-    return 0;
+int main(int argc, char **argv) {
+	printf("%ld\n", fibo(atol(argv[1])));
+	return 0;
 }
--- fibo.cpp
+++ fibo.cpp
@@ -1,20 +1,17 @@
 #include <iostream>
 #include <sstream>
 
-unsigned long
-fibo (unsigned long n )
-{
-    if (n < 2)
-        return n;
-    else
-        return fibo(n-1) + fibo(n-2);
+unsigned long fibo(unsigned long n) {
+	if (n < 2) {
+		return n;
+	} else {
+		return fibo(n - 1) + fibo(n - 2);
+	}
 }
 
-int
-main(int argc, char** argv)
-{
-    unsigned long lim;
-    std::stringstream(argv[1]) >> lim;
-    std::cout << fibo(lim) << std::endl;
-    return 0;
+int main(int argc, char **argv) {
+	unsigned long lim;
+	std::stringstream(argv[1]) >> lim;
+	std::cout << fibo(lim) << std::endl;
+	return 0;
 }
